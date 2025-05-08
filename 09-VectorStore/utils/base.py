7bce9cce9add33a53e7802cb76867a0548813f5e
SOHYEON <brightcattle@gmail.com>
2025-05-08 23:18:26 +0900

    refactor: remove duplicate DocumentManager class definition

--- 09-VectorStore/utils/base.py
+++ 09-VectorStore/utils/base.py
@@ -1,75 +1,7 @@
 from abc import ABC, abstractmethod
-from typing import Any, Dict, Optional, List, Iterable
+from typing import Any, Dict, Optional, List, Iterable, Callable
 from langchain_core.documents import Document
-
-
-class DocumentManager(ABC):
-    """
-    Document insert/update (upsert, upsert_parallel)
-    Document search by query (search)
-    Document delete by id, delete by filter (delete)
-    """
-
-    @abstractmethod
-    def upsert(
-        self,
-        texts: Iterable[str],
-        metadatas: Optional[list[dict]] = None,
-        ids: Optional[List[str]] = None,
-        **kwargs: Any
-    ) -> None:
-        """Upsert Document"""
-        pass
-
-    @abstractmethod
-    def upsert_parallel(
-        self,
-        texts: Iterable[str],
-        metadatas: Optional[list[dict]] = None,
-        ids: Optional[List[str]] = None,
-        batch_size: int = 32,
-        workers: int = 10,
-        **kwargs: Any
-    ) -> None:
-        """Upsert Document Parallel"""
-        pass
-
-    @abstractmethod
-    def search(self, query: str, k: int = 10, **kwargs: Any) -> List[Document]:
-        """Performs a query and returns relevant documents.
-        Basic function: query (string) -> return k similar documents
-
-        Meaning to search cosine_similarity **If a problem arises, raise an issue
-
-        -Other features (future extensions)
-        metatdata search
-        Getting a vector when searching for an image
-        """
-        pass
-
-    @abstractmethod
-    def delete(
-        self,
-        ids: Optional[list[str]] = None,
-        filters: Optional[dict] = None,
-        **kwargs: Any
-    ) -> None:
-        """Use a filter to delete the document.
-
-        IDS: List of IDs to delete. Delete all if none. Default is none.
-        Filter: filter (query) dictionary to be applied; no filter will be applied.
-
-        """
-        pass
-
-
-"""
-New Interface for VectorDB CRUD
-"""
-
-from typing import Optional, List, Iterable, Any, Dict, Callable
 from langchain_core.retrievers import BaseRetriever
-from langchain_core.documents import Document
 from pydantic import Field
 
 
